f0cfeefe320fc59994f168482814ecf0f33973f0
TheTechGrandma <tadkinsmcclellan@gmail.com>
2016-04-13 16:22:42 -0400

    Added files via upload

--- index.js
+++ index.js
@@ -10,4 +10,36 @@ app.launch(function(req, res){
 	res.say(prompt).reprompt(prompt).shouldEndSession(false);
 });
 
-module.exports.app;
+app.intent('alexaWhereIsIt',{
+	'slots': {
+		'AIRPORTCODE': 'FAACODES'
+	},
+	'utterances': ['{|flight|airport} {|delay|status} {|info} {|for} {-|AIRPORTCODE}']
+},
+	function(req, res){
+		//get the slot
+		var airport = req.slot('AIRPORTCODE');
+		var reprompt = 'Tell me an airport code to get delay information.';
+
+		if (_.isEmpty(airportCode)){
+			var prompt = 'I didn\'t hear an airport code. Tell me an airport code.';
+			res.say(prompt).reprompt(reprompt).shouldEndSession(false);
+			return true;
+		}
+		else
+		{
+			var whereHelper = new WhereDataHelper();
+			whereHelper.requestAirportStatus(airportCode).then(function(airportStatus) {
+				console.log(airportStatus);
+				res.say(whereHelper.formatAirportStatus(airportStatus)).send();
+			}).catch (function(err) {
+				console.log(err.statusCode);
+				var prompt = 'I didn\'t have data for an airport code of ' + airportCode;
+				res.say(prompt).reprompt(reprompt).shouldEndSession(false).send();
+			});
+			return false;
+		}
+	}
+);
+
+module.exports = app;
